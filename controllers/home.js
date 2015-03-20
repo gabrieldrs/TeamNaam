34cf9d958cc3ad0058c53577420f177a880f480a
Gabriel Sezefredo <gabriel@sezefredo.com.br>
2015-03-19 19:25:12 -0700

    Poorly listing mentors on staging page

--- controllers/home.js
+++ controllers/home.js
@@ -60,12 +60,13 @@ exports.juniorMatching = function(req, res) {
 
 
 exports.staging = function(req, res) {
-  Application.find({ cohort: res.locals.activeCohort , student: true }).lean().exec( function(err, applications){
+  Application.find({ cohort: res.locals.activeCohort}).lean().exec( function(err, applications){
     if (err){ 
       console.error(err);
       req.flash('errors', { msg: 'Failed to retrieve applications.' });
     }
-    
+    var studentList = [];
+    var mentorList = [];
     applications.forEach(function(app){    
         var yearsDOB  = moment().diff(app['Birth Date'], 'years');  // Gets remainder of weeks, converted to decimal, truncated to one decimal place.
         var weeksDOB  = Math.floor( moment().diff(app['Birth Date'], 'weeks') %52/5.2);
@@ -73,13 +74,19 @@ exports.staging = function(req, res) {
         app.name=app['First Name']+' '+ app['Last Name']
         app.age = yearsDOB +'.'+ weeksDOB +' years old'
         app.submissionMoment =moment(app.submissionDate).fromNow();
+        if (app.student){
+          studentList.push(app);
+        }else{
+         mentorList.push(app); 
+        }
     }); 
     console.log(applications);
     
         
     res.render('pages/staging', {
       title: 'Staging',
-      applications: applications
+      studentApp: studentList,
+      mentorApp: mentorList
     }); 
   });
 };
@@ -90,23 +97,22 @@ exports.staging = function(req, res) {
  */
 
 exports.explorer = function(req, res) {
-    var cid=res.locals.cohort;
-    console.log(cid);
-    Cohort.findById(cid).lean().exec(function( err, cohort){
-        if (formData) {
-            var formData = formLoader.getForm(cohort.form);
-            var factors = formData.map(function (el) {
-                return {
-                    shortName: el.shortName,
-                    weight: el.weight
-                };
-            });
-        }
-        console.log(factors);
-        res.render('pages/explorer', {
-            title: 'Home',
-            factors: factors
+    var cohort=res.locals.cohort;
+    var formData = formLoader.getForm(cohort.form);
+    if (formData) {
+        var factors = formData.map(function (el) {
+            return {
+                shortName: el.shortName,
+                weight: el.weight,
+                analyze:el.analyze,
+                field:el.analyzeField
+            };
         });
+    }
+  console.log(factors);
+    res.render('pages/explorer', {
+        title: 'Home',
+        factors: factors
     });
 };
 /*
