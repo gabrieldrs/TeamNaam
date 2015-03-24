532ded59b85db6e5e93029ad67375ebfc3acc4fb
mcgrandlej <mcgrandlej@gmail.com>
2015-03-24 13:11:10 -0700

    Add a few comments

--- controllers/algorithm.js
+++ controllers/algorithm.js
@@ -4,9 +4,9 @@ var _ = require('lodash');
 
 
 exports.setWeights = function(req, res) {
-  
+
   var factors=req.body.factors;
-  
+
   Application.find({ cohort: res.locals.activeCohort, accepted: true }).lean().exec(function(err, applications) {
     var matchings = calcTrioMatching(applications,factors);  // This is an optional helper function
     res.status(200).json(matchings);
@@ -18,10 +18,10 @@ function calcTrioMatching(applications,factors){
   var juniors = _.where(applications, {student:true,senior:false});
   var seniors = _.where(applications, {student:true,senior:true});
   var mentors = _.where(applications, {student:false});
-  
+
   var mentorMatch = calcDuoMatching(mentors,seniors,factors);
   var finalMatch = calcDuoMatching(seniors,juniors,factors,mentorMatch);
-  
+
   console.log(finalMatch);
   return finalMatch;
 }
@@ -32,7 +32,7 @@ function calcDuoMatching(groupOne,groupTwo,factors,currentMatch){
   if (currentMatch == undefined){
     return calcDuoMatching2(groupOne,groupTwo,factors);
   }
-  
+
   if (groupOne.length < 1 || groupTwo.length < 1)
     return currentMatch;
   var groupOneName =  getGroupName(groupOne[0]);
@@ -43,9 +43,9 @@ function calcDuoMatching(groupOne,groupTwo,factors,currentMatch){
     });
     groupOne[i]["availability"] = match['availability']; // overwriting it's availability, so it matches with the currentMatch's ones.
   });
-  
+
   var groupOneMatch = calcDuoMatching(groupOne,groupTwo,factors);
-  
+
   currentMatch.forEach(function(match){
     var i = _.findIndex(groupOneMatch, function(appOne){
       return appOne[groupOneName+'ID'] == match[groupOneName+'ID'];
@@ -54,9 +54,9 @@ function calcDuoMatching(groupOne,groupTwo,factors,currentMatch){
     match['quality'] = (match['quality'] + groupOneMatch[i]['quality'])/2;
     match["availability"] = _.intersection(match['availability'],groupOneMatch[i]['availability']);
   });
-  
+
   return currentMatch;
-  
+
 }
 
 function calcDuoMatching2(groupOne,groupTwo,factors){
@@ -64,22 +64,22 @@ function calcDuoMatching2(groupOne,groupTwo,factors){
     return {};
   var appOneMatch = [];
   var tempAppOneMatch = [];
-  
+
   // These are used to label the JSON fields
   var groupOneName = getGroupName(groupOne[0]);
   var groupTwoName = getGroupName(groupTwo[0]);
-  
-  groupOne.forEach(function(appOne){
+
+  groupOne.forEach(function(appOne){ //Cycle through the first group (mentors, then seniors)
     var thisMatch = [];
-    groupTwo.forEach(function(appTwo){
-      
-      var appOneAvail = convertToArray(appOne["availability"]);
-      var appTwoAvail = convertToArray(appTwo['availability']);
-      var commonAvail = _.intersection(appOneAvail,appTwoAvail);
-      
+    groupTwo.forEach(function(appTwo){ //Cycle through the second group (seniors, juniors)
+
+      var appOneAvail = convertToArray(appOne["availability"]); //The first groups availability
+      var appTwoAvail = convertToArray(appTwo['availability']); //The second groups availability
+      var commonAvail = _.intersection(appOneAvail,appTwoAvail); //Nights that both applicants share
+
       if (commonAvail.length == 0)
-        return;
-      var quality = calcMatchQuality(appOne,appTwo,factors);
+        return; //They have no nights in common, they can't be paired
+      var quality = calcMatchQuality(appOne,appTwo,factors); //Calculate the match success of these two applicants
       var thisMatchEntry = JSON.parse("{"+
       '"'+groupTwoName+'ID" : "'+appTwo['_id']+'",'+
       '"quality" : '+quality+","+
@@ -87,7 +87,7 @@ function calcDuoMatching2(groupOne,groupTwo,factors){
       "}")
       thisMatchEntry['availability'] = commonAvail;
       thisMatch.push(thisMatchEntry);
-      
+
     });
     if (thisMatch.length > 0){
       //console.log(thisMatch);
@@ -116,7 +116,7 @@ function calcDuoMatching2(groupOne,groupTwo,factors){
     matchEntry['availability'] = bestMatch['availability']
     appOneMatch.push(matchEntry);
   });
-  
+
   return appOneMatch;
 }
 
@@ -134,12 +134,12 @@ function calcMatchQuality(user1,user2,factors){
       factorCount++;
     else
       firstHalf = 0;
-    
+
     if (secondHalf != -1)
       factorCount++;
     else
       secondHalf = 0;
-    
+
     quality += (firstHalf+secondHalf);
   });
   if (factorCount>0)
@@ -150,13 +150,13 @@ function calcMatchQuality(user1,user2,factors){
 
 function calcThisFactor(user1,user2,factor){
   var thisQuality = 0;
-  
+
   //If there are any undefined analyzeRefs, return -1
   if (!analyzeRefExists(user1,factor['name']) || !analyzeRefExists(user2,factor['analyzeRef']))
     return -1;
   user1[factor['name']] = convertToArray(user1[factor['name']]);
   user2[factor['analyzeRef']] = convertToArray(user2[factor['analyzeRef']]);
-  
+
   user1[factor['name']].forEach(function(v1){
     user2[factor['analyzeRef']].forEach(function(v2){
       if (v1 == v2){
