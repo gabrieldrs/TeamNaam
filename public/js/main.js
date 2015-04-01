67a0a59ec01fd62e5241ea5154e54617bc8ba9e0
John G <JAGracey@gmail.com>
2015-04-01 16:22:02 -0700

    Ended up breaking the viewport for the scrollReveal page transitions. Here is a fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
   // Place JavaScript code here...
-  window.sr = new scrollReveal({vFactor: 0, mobile: true, scale: {}, viewport: window });
+  window.sr = new scrollReveal({vFactor: 0, mobile: true, viewport: document.getElementById('sidebarViewRight'), scale: {direction: 'right', power: '0%'} });
+  
 });
