f4ad6ec9e6e840044ab5b41c83e8b5d16c951173
Gabriel Sezefredo <gabriel@sezefredo.com.br>
2015-04-01 16:37:38 -0700

    Revert "fixed viewport listener issue with page transitions"
    This reverts commit 0d6b32e75a07c7a1cfb8b736096c04efa9ddf036.
    
    Conflicts:
    	public/js/main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
   // Place JavaScript code here...
-  window.sr = new scrollReveal({vFactor: 0, mobile: true, viewport: document.getElementById('sidebarViewRight'), scale: {direction: 'right', power: '0%'} });
-  
+  window.sr = new scrollReveal({vFactor:  0, mobile:   true});
 });
